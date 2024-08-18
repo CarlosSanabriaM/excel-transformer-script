e5d513f6590adf49c240f7ee03f4338899bb20d6
CarlosSanabriaM <sanabria.miranda.carlos@gmail.com>
2024-08-18 12:25:05 +0200

    refactor: use strategy design pattern for the transformation algorithm

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
 import openpyxl
-from openpyxl.styles import Font
 
+from strategies.set_zero_when_count_lower_than_absolute_value_strategy import SetZeroWhenNumberLowerThanAbsoluteValueStrategy
 
-def transform_excel(input_file, output_file, start_column_name):
+STRATEGIES_MAP = {
+    'ABSOLUTE': SetZeroWhenNumberLowerThanAbsoluteValueStrategy(),
+}
+
+def transform_excel(transform_strategy, input_file, output_file, start_column_name):
     # Load the workbook and select the active sheet
     wb = openpyxl.load_workbook(input_file)
     sheet = wb.active
@@ -17,24 +21,9 @@ def transform_excel(input_file, output_file, start_column_name):
     if start_column_index is None:
         raise ValueError(f"Column '{start_column_name}' not found in the Excel file.")
 
-    # Define the red font
-    red_font = Font(color="FF0000")
-
-    # Iterate over the rows starting from the second row
-    for row in range(2, sheet.max_row + 1):
-        col = start_column_index
-        while col < sheet.max_column:
-            int_value_cell = sheet.cell(row=row, column=col)
-            perc_value_cell = sheet.cell(row=row, column=col + 1)
-
-            # Check if the integer value is less than 100
-            if int_value_cell.value is not None and int_value_cell.value < 100:
-                int_value_cell.value = 0
-                int_value_cell.font = red_font  # Set the text color to red
-                perc_value_cell.value = 0
-                perc_value_cell.font = red_font  # Set the text color to red
-
-            col += 2  # Move to the next pair of columns
+    # Use the selected strategy to transform the sheet
+    strategy = STRATEGIES_MAP.get(transform_strategy)
+    strategy.transform_excel_sheet(sheet, start_column_index)
 
     # Save the modified workbook preserving the formatting
     wb.save(output_file)
@@ -43,8 +32,9 @@ def transform_excel(input_file, output_file, start_column_name):
 
 if __name__ == "__main__":
     # Update these values
+    transform_strategy = 'ABSOLUTE'  # Replace with your desired strategy
     input_excel_file = 'input.xlsx'  # Replace with your input file name
     output_excel_file = 'output.xlsx'  # Replace with your desired output file name
     start_column_name = 'First column to modify'  # Replace with your specific start column name
 
-    transform_excel(input_excel_file, output_excel_file, start_column_name)
+    transform_excel(transform_strategy, input_excel_file, output_excel_file, start_column_name)
---
+++ strategies/transform_strategy.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from openpyxl.styles import Font
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+class TransformStrategy(ABC):
+    """
+    Interface for all supported versions of the transform algorithm over the input Excel sheet.
+    """
+
+    # Define the red font
+    red_font = Font(color="FF0000")
+
+    @abstractmethod
+    def transform_excel_sheet(self, sheet: Worksheet, start_column_index: int) -> None:
+        pass
